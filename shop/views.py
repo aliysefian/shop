bda8f694f2e8f4294883f8adb4bb6529c0cf135d
aliysefian <ali1391ali1391@yahoo.com>
2019-09-15 19:01:32 +0430

    add bootstrap

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
+from shop.models import Category, Product
+
+
+def product_list(requset, category_slug=None):
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = Product.objects.filter(category=category)
+    return render(requset, 'shop/product/list.html', {'categories': categories, 'products': products})
